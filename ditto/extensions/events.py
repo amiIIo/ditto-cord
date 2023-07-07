af94bd1e8c0aeae2061e29f16ba4dc56976a15db
amillo <79035408+amillolive@users.noreply.github.com>
2023-07-07 02:13:43 -0400

    Established an error handler (+ bonus))
    Update!
    - error handler
    - 'close' command (ease of use)

--- ditto/extensions/events.py
+++ ditto/extensions/events.py
@@ -9,7 +9,21 @@ async def on_started(event: hikari.StartedEvent):
     bot = plugin.app
     print(f"----\nditto-cord is running.\nCLIENT-ID: 1126714035782557769\n----")
     stdout = await bot.rest.fetch_channel(OUTPUT_KEY)
-    await stdout.send("I'm awake! How can I assist you?")
 
+@plugin.listener(lightbulb.CommandErrorEvent)
+async def on_error(event: lightbulb.CommandErrorEvent) -> None:
+    if isinstance(event.exception, lightbulb.CommandInvocationError):
+        await event.context.respond(f"Yikes! Something weird happened when you tried to use `{event.context.command.name}`.")
+        raise event.exception
+
+    exception = event.exception.__cause__ or event.exception
+
+    if isinstance(exception, lightbulb.NotOwner):
+        await event.context.respond("Whoops! Only the developer is authorized to use the command.")
+    elif isinstance(exception, lightbulb.CommandIsOnCooldown):
+        await event.context.respond(f"Woah! This command is on cooldown. Retry in `{exception.retry_after:.2f}` seconds.")
+    else:
+        raise exception
+    
 def load(bot):
     bot.add_plugin(plugin)
--- ditto/extensions/utilities.py
+++ ditto/extensions/utilities.py
@@ -5,10 +5,20 @@ from ditto import OUTPUT_KEY
 plugin = lightbulb.Plugin('Utilities')
 
 @plugin.command
+@lightbulb.add_cooldown(3, 1, lightbulb.UserBucket)
 @lightbulb.command('ping', 'Responds with the latency of the app.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping(ctx):
     await ctx.respond(f"Pong! My latency was roughly {round(plugin.bot.heartbeat_latency*1000)}ms.")
+
+@plugin.command
+@lightbulb.add_checks(lightbulb.owner_only)
+@lightbulb.add_cooldown(5, 1, lightbulb.UserBucket)
+@lightbulb.command('close', 'Close the connection to discord.')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def close(ctx):
+    await ctx.respond("Closing the connection. - Ditto")
+    quit()
 
 def load(bot):
     bot.add_plugin(plugin)
